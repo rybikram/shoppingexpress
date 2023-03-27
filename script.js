1966e33657089a79034333114f80cbac2bef0513
rybikram <125821562+rybikram@users.noreply.github.com>
2023-03-27 22:45:12 +0530

    shoppingexpress

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// For hamburger
+const bar = document.getElementById('bar')
+const close = document.getElementById('close')
+
+const nav = document.getElementById('navbar')
+
+if(bar){
+    bar.addEventListener('click', function(){
+        nav.classList.add('active')
+    })
+}
+
+if(close){
+    close.addEventListener('click', function(){
+        nav.classList.remove('active')
+    })
+}
+
+
+
+// For single products zooming
+
